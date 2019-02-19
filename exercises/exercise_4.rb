df9e02894fd7a350c4f4d2cb8feb2754ff5099e3
cepholopoddreamz <leslie.plumb@gmail.com>
2019-02-19 18:16:32 +0000

    excercie 4 complete note that at firest i tried to use find  but that only returns the first instance of an encountered true query you need to use where to return a collection

--- exercises/exercise_4.rb
+++ exercises/exercise_4.rb
@@ -7,3 +7,29 @@ puts "Exercise 4"
 puts "----------"
 
 # Your code goes here ...
+require_relative '../setup'
+
+puts "Exercise 1"
+puts "----------"
+
+Store.create! name: "Surrey", annual_revenue: " 224000", mens_apparel: false, womens_apparel: true
+
+Store.create! name: "Whistler", annual_revenue: "1900000", mens_apparel: true, womens_apparel: false
+
+Store.create! name: "Yaletown", annual_revenue: "430000", mens_apparel: true, womens_apparel: true
+
+@mens_stores = Store.where(mens_apparel: true)
+
+# puts @mens_stores
+# puts @mens_stores.inspect
+
+#binding.pry
+
+@mens_stores.each do |n|
+  puts n.name
+  puts n.annual_revenue
+end
+
+
+
+
